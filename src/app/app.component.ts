bbbc68d73f37049c980a7a4c5a70701dc2a85709
Jithinraj b <Jithinraj.Badi@ust.com>
2022-11-13 16:20:13 +0530

    changing and adding header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,65 +1,12 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'sevens';
-  url:string = "http://localhost:8989/currentGame";
-
-  team1:string = "";
-  team2:String = "";
-  fixt:String = "";
-
-
-  team3:string = "";
-  team4:String = "";
-  fixt2:String = "";
-
-  team5:string = "";
-  team6:String = "";
-  fixt3:String = "";
-
-  team7:string = "";
-  team8:String = "";
-  fixt4:String = "";
-
-  constructor(private http: HttpClient) { }
-
-  ngOnInit(): void {
-   this.getDetails();
-  }
-
-  getDetails(){
-    console.log("inside dets");
-    this.http.get<any>(this.url).subscribe(data=>{
-
-      this.team1 = data.matches[0].homeTeam.tla;
-      this.team2 = data.matches[0].awayTeam.tla;
-      this.fixt = data.matches[0].competition.name;
-
-      this.team3 = data.matches[1].homeTeam.tla;
-      this.team4 = data.matches[1].awayTeam.tla;
-      this.fixt2 = data.matches[1].competition.name;
-
-      this.team5 = data.matches[2].homeTeam.tla;
-      this.team6 = data.matches[2].awayTeam.tla;
-      this.fixt3 = data.matches[2].competition.name;
-
-      this.team7 = data.matches[3].homeTeam.tla;
-      this.team8 = data.matches[3].awayTeam.tla;
-      this.fixt4 = data.matches[3].competition.name;
-
-      console.log(data.matches);
-    },error=>{
-      console.log("damn it eror");
-    })
-
-
-  }
-
+export class AppComponent{
+  title = 'Sevens';
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
